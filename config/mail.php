91495b09623bbe8cf586fd1aad37235ff7f3f926
Shingo Ono (Mim) <shingo.0416@mim-hd.co.jp>
2026-07-16 00:25:54 +0900

    mail調整

--- config/mail.php
+++ config/mail.php
@@ -39,7 +39,11 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
+            'scheme' => match (env('MAIL_SCHEME', env('MAIL_ENCRYPTION'))) {
+                'tls', 'starttls' => 'smtp',
+                'ssl' => 'smtps',
+                default => env('MAIL_SCHEME'),
+            },
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
